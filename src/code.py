c71b5b892f281938d33507e4358144525120c447
djayinski <58003211+djayinski@users.noreply.github.com>
2019-12-16 13:18:47 -0500

    colors

--- src/code.py
+++ src/code.py
@@ -4,9 +4,17 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
+# create colors
+light_blue = pygame.Color(173, 216, 230)
+navy = pygame.Color(2, 7, 93)
+
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 350) )
 print(pygame.QUIT)
+
+#make screen change color over time
+
+
 # Main loop. Your game would go inside this loop
 while True:
     # do something for each event in the event queue (list of things that happen)
