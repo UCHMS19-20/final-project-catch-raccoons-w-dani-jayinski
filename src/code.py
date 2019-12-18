dc94aeee8f44bafcdb6a8d63039fed606fd672fc
djayinski <58003211+djayinski@users.noreply.github.com>
2019-12-18 12:19:17 -0500

    made color changing screen

--- src/code.py
+++ src/code.py
@@ -1,22 +1,28 @@
 import sys
 import pygame
+from pygame.math import Vector3
 
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
 # create colors
-light_blue = pygame.Color(173, 216, 230)
-navy = pygame.Color(2, 7, 93)
-
+light_blue = Vector3(173, 216, 230)
+navy = Vector3(2, 7, 93)
+# make it change slowly
+color_counter = 0
+color_step = 0.00002
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 350) )
 print(pygame.QUIT)
 
-#make screen change color over time
 
 
 # Main loop. Your game would go inside this loop
 while True:
+    if color_counter < 1:
+        color_counter += color_step
+        if color_counter > 1:
+            color_counter = 1
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
 
@@ -27,3 +33,6 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+    # make screen change color over time
+    screen.fill(navy.lerp(light_blue, color_counter))
+    pygame.display.flip()
