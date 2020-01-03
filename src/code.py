91880966b10ba3a76ab134ee25ca74858619da6d
djayinski <58003211+djayinski@users.noreply.github.com>
2020-01-03 10:43:02 -0500

    tried to put it back to how it was

--- src/code.py
+++ src/code.py
@@ -1,14 +1,14 @@
 import sys
 import pygame
 import random
-
+from pygame.math import Vector3
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
 # create colors
-light_blue = pygame.Color(173, 216, 230)
-navy = pygame.Color(2, 7, 93)
-white = pygame.Color(255, 255, 255)
+light_blue = Vector3(173, 216, 230)
+navy = Vector3(2, 7, 93)
+white = Vector3(255, 255, 255)
 # make it change slowly
 color_counter = 0
 # color_step = 0.00003
@@ -21,73 +21,25 @@ print(pygame.QUIT)
 sun = pygame.image.load("src/img/Sun_with_sunglasses[1].jpg")
 raccoon = pygame.image.load("src/img/raccoon-revised-md[1].png")
 
-#make a font object for title/instructions
+#make a font object
 font = pygame.font.Font('freesansbold.ttf', 32) 
-text = font.render('Catch the raccoons before daytime!', True, white) 
-
-
- # to make raccoons pop up at locations
-class Raccoon_popup:
-    def __init__(self):
-        #width of raccoon
-        self.RACCOON_WIDTH = 90
-        self.RACCOON_HEIGHT = 81
-        #set score starting at zero 
-        self.score = 0
-        # Font object for displaying score text
-        self.FONT_SIZE = 31
-        self.font_obj = pygame.font.Font('./fonts/GROBOLD.ttf', self.FONT_SIZE)
-        #make raccoons pop up
-        self.raccoons = []
-        self.raccoons.append(raccoon.subsurface(169, 0, 90, 81))
-        self.raccoons.append(raccoon.subsurface(309, 0, 90, 81))
-        self.raccoons.append(raccoon.subsurface(449, 0, 90, 81))
-        self.raccoons.append(raccoon.subsurface(575, 0, 116, 81))
-        self.raccoons.append(raccoon.subsurface(717, 0, 116, 81))
-        self.raccoons.append(raccoon.subsurface(853, 0, 116, 81))
-        
-    # Check whether the mouse click caught the raccoon or not
-    def is_raccoon_caught(self, mouse_position, current_hole_position):
-        mouse_x = mouse_position[0]
-        mouse_y = mouse_position[1]
-        current_hole_x = current_hole_position[0]
-        current_hole_y = current_hole_position[1]
-        if (mouse_x > current_hole_x) and (mouse_x < current_hole_x + self.RACCOON_WIDTH) and (mouse_y > current_hole_y) and (mouse_y < current_hole_y + self.RACCOON_HEIGHT):
-            return True
-        else:
-            return False
-    #update score
-    def update_score(self):
-        current_score_string = "SCORE: " + str(self.score)
-        score_text = self.font_obj.render(current_score_string, True, (255, 255, 255))
-        screen.blit(score_text, (10,500))
-
-
-    for event in pygame.event.get():
-            if event.type == MOUSEBUTTONDOWN and event.button == self.LEFT_MOUSE_BUTTON:
-                    
-                if self.is_raccoon_caught(mouse.get_pos(), self.hole_positions[frame_num]) and num > 0 and left == 0:
-                        num = 3
-                        left = 14
-                        is_down = False
-                        interval = 0
-                        self.score += 1  # Increase player's score
+text = font.render('Catch the Raccoons before daytime!', True, white) 
    
 # Main loop. Your game would go inside this loop
-    while True:
-        # make screen change color over time
-        screen.fill(navy.lerp(light_blue, color_counter))
-        # make text appear 
-        screen.blit(text, (200,10))
-        # make sun pop up
-        if color_counter == 1:
-            screen.blit(sun, (700,50))
+while True:
+    # make screen change color over time
+    screen.fill(navy.lerp(light_blue, color_counter))
+    # make text appear 
+    screen.blit(text, (200,10))
+    # make sun pop up
+    if color_counter == 1:
+        screen.blit(sun, (700,50))
     
     #to change color of background in increments and then stop
-        if color_counter < 1:
-            color_counter += color_step
-            if color_counter > 1:
-                color_counter = 1
+    if color_counter < 1:
+        color_counter += color_step
+        if color_counter > 1:
+            color_counter = 1
     
   
    
@@ -95,8 +47,14 @@ class Raccoon_popup:
 #WHAT I NEED TO DO:
 # figure out how to fix the raccoons popping up,
 # figure out if a raccooon is clicked
-
 # keep track of and display how many were clicked on
+   
+    # to make raccoons pop up at random locations
+    for x in range(30):
+        screen.blit(raccoon, (random.randint(1,801), random.randint(1,801)))
+    # do something for each event in the event queue (list of things that happen)
+    for event in pygame.event.get():
+
         # This line will print each event to the terminal
             print(event)
 
