312b559dc986a4fef5812e9b48a5fe5350437f87
djayinski <58003211+djayinski@users.noreply.github.com>
2020-01-06 13:21:13 -0500

    whack a mole good

--- src/code.py
+++ src/code.py
@@ -11,8 +11,8 @@ navy = Vector3(2, 7, 93)
 white = Vector3(255, 255, 255)
 # make it change slowly
 color_counter = 0
-# color_step = 0.00003
-color_step = .0005
+
+color_step = .00003
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (1000, 500) )
 print(pygame.QUIT)
@@ -20,11 +20,27 @@ print(pygame.QUIT)
 #images used
 sun = pygame.image.load("src/img/Sun_with_sunglasses[1].jpg")
 raccoon = pygame.image.load("src/img/raccoon-revised-md[1].png")
-
+r = raccoon.get_rect()
 #make a font object
 font = pygame.font.Font('freesansbold.ttf', 32) 
 text = font.render('Catch the Raccoons before daytime!', True, white) 
-   
+
+# x and y of raccoons
+x = 300
+y = 100
+#create timer event
+clock = pygame.time.Clock()
+TIMEREVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(TIMEREVENT, 2000)
+
+def did_catch_raccoon():
+    """figure out if raccoon was clicked on"""
+    pass
+
+def update_score():
+    """display number of raccoons caught"""
+    pass
+    
 # Main loop. Your game would go inside this loop
 while True:
     # make screen change color over time
@@ -41,28 +57,32 @@ while True:
         if color_counter > 1:
             color_counter = 1
     
-  
-   
-   
-#WHAT I NEED TO DO:
-# figure out how to fix the raccoons popping up,
-# figure out if a raccooon is clicked
-# keep track of and display how many were clicked on
-   
-    # to make raccoons pop up at random locations
-    for x in range(30):
-        screen.blit(raccoon, (random.randint(1,801), random.randint(1,801)))
+
+
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        if event.type == TIMEREVENT:
+            x = random.randint(0, 1000 - raccoon.get_width())
+            y = random.randint(0, 500 - raccoon.get_height())
+            r.x = x
+            r.y = y
 
+            r = raccoon.get_rect()
         # This line will print each event to the terminal
             print(event)
-
         # Check to see if the current event is a QUIT event
-            if event.type == pygame.QUIT:
-            # If so, exit the program
-                sys.exit()
-  
+        if event.type == pygame.QUIT:
+        # If so, exit the program
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if r.collidepoint(pygame.mouse.get_pos()):
+                x = random.randint(0, 1000 - raccoon.get_width())
+                y = random.randint(0, 500 - raccoon.get_height())
+                r.x = x
+                r.y = y
+                
+        
+    screen.blit(raccoon,(x,y))
     pygame.display.flip()
     
         
